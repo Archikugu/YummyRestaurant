8a7f0fcb3d91c5df32d1346a40caeec2410a0c18
Archikugu <engokhangok@gmail.com>
2026-01-06 17:14:16 +0300

    Implement dynamic Contact section with API integration

--- Presentation/YummyRestaurant.WebUI/ViewComponents/DefaultComponents/DefaultContactViewComponent.cs
+++ Presentation/YummyRestaurant.WebUI/ViewComponents/DefaultComponents/DefaultContactViewComponent.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using YummyRestaurant.Application.DTOs.ContactDTOs;
 
 namespace YummyRestaurant.WebUI.ViewComponents.DefaultComponents
 {
-    public class DefaultContactViewComponent : ViewComponent
+    public class DefaultContactViewComponent(IHttpClientFactory _httpClientFactory) : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7182/api/Contacts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+                return View(values.FirstOrDefault()); // Return the first contact or null
+            }
             return View();
         }
     }
